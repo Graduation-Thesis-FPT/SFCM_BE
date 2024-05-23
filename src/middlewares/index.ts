9ab97f3dbbce5c5fdead7fa985906c7e436f79e6
Vu Anh Lam <vuanhlam000@gmail.com>
2024-05-23 17:31:59 +0700

    Define type for readability.

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -7,8 +7,11 @@ const HEADER = {
   MENU_CODE: 'menu-code',
 };
 
+type HttpMethod = 'GET' | 'POST' | 'DELETE' | 'PATCH';
+type PermissionProperty = 'IS_VIEW' | 'IS_ADD_NEW' | 'IS_DELETE' | 'IS_MODIFY';
+
 const grantPermission = async (req: Request, res: Response, next: NextFunction) => {
-  const methodPermissionMap = {
+  const methodPermissionMap: Record<HttpMethod, PermissionProperty> = {
     GET: 'IS_VIEW',
     POST: 'IS_ADD_NEW',
     DELETE: 'IS_DELETE',
@@ -18,11 +21,11 @@ const grantPermission = async (req: Request, res: Response, next: NextFunction) 
   const { ROLE_CODE } = res.locals.user;
   const menuCode = req.headers[HEADER.MENU_CODE] as string;
 
-  if(!menuCode) {
-    throw new BadRequestError('Missing menuCode!')
+  if (!menuCode) {
+    throw new BadRequestError('Missing menuCode!');
   }
 
-  const method = req.method as 'GET' | 'POST' | 'DELETE' | 'PATCH';
+  const method = req.method as HttpMethod;
 
   const permission = await checkPermissionAccessMenu(ROLE_CODE, menuCode);
 
@@ -30,7 +33,7 @@ const grantPermission = async (req: Request, res: Response, next: NextFunction) 
     throw new BadRequestError('You do not have permission to access this page!');
   }
 
-  const permissionProperty = methodPermissionMap[method] as 'IS_VIEW' | 'IS_ADD_NEW' | 'IS_DELETE' | 'IS_MODIFY';
+  const permissionProperty = methodPermissionMap[method] as PermissionProperty;
 
   if (!permission[permissionProperty]) {
     throw new BadRequestError('You do not have this permission');
