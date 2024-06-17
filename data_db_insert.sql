42499246a95d2e26995642af15296a582094259d
PenVsse <vusiphu20042000@gmail.com>
2024-06-18 00:59:20 +0700

    fix file .sql

---
+++ data_db_insert.sql
@@ -0,0 +1,67 @@
+INSERT INTO SA_ROLE (ROLE_CODE, ROLE_NAME,CREATE_BY,UPDATE_BY)
+VALUES ('admin',N'Quản trị viên','sql','sql'),
+('procedure-staff',N'Thủ tục','sql','sql'),
+('gate-operator',N'Điều hành cổng','sql','sql'),
+('tally-operator',N'Điều hành kiểm đếm','sql','sql'),
+('warehouse-operator',N'Điều hành kho','sql','sql')
+
+INSERT INTO SA_MENU (PARENT_CODE,MENU_CODE,MENU_NAME,MENU_ICON,IS_VISIBLE,ORDER_BY,VIEW_PAGE,CREATE_BY,UPDATE_BY)
+VALUES 
+(NULL,'user-management',N'Quản lý người dùng','Users',1,100,NULL,'sql','sql'),
+('user-management','user',N'Người dùng',NULL,1,101,'User','sql','sql'),
+('user-management','permission',N'Phân quyền',NULL,1,102,'Permission','sql','sql'),
+(NULL,'generic-list',N'Danh mục dùng chung','List',1,200,NULL,'sql','sql'),
+('generic-list','warehouse-list',N'Danh mục kho',NULL,1,201,'WarehouseList','sql','sql'),
+('generic-list','warehouse-design',N'Thiết kế kho',NULL,1,203,'WarehouseDesign','sql','sql'),
+('generic-list','gate-list',N'Danh mục cổng',NULL,1,204,'GateList','sql','sql'),
+('generic-list','equipment-group-list',N'Danh mục loại thiết bị',NULL,1,205,'EquipmentGroupList','sql','sql'),
+('generic-list','equipment-list',N'Danh mục thiết bị',NULL,1,206,'EquipmentList','sql','sql'),
+('generic-list','method-list',N'Danh mục phương án',NULL,1,207,'MethodList','sql','sql'),
+('generic-list','item-type',N'Danh mục loại hàng hóa',NULL,1,208,'ItemType','sql','sql'),
+('generic-list','unit-list',N'Danh mục đơn vị tính',NULL,1,209,'UnitList','sql','sql'),
+('generic-list','customer-type',N'Danh mục loại khách hàng',NULL,1,210,'CustomerType','sql','sql'),
+('generic-list','customer-list',N'Danh mục khách hàng',NULL,1,211,'CustomerList','sql','sql'),
+('generic-list','tractor-list',N'Danh mục đầu kéo',NULL,0,212,'TractorList','sql','sql'),
+('generic-list','trailer-list',N'Danh mục rơ-mooc',NULL,0,213,'TrailerList','sql','sql'),
+(NULL,'input-data',N'Dữ liệu đầu vào','FolderInput',1,300,NULL,'sql','sql'),
+('input-data','vessel-info',N'Thông tin tàu chuyến',NULL,1,301,'VesselInfo','sql','sql'),
+('input-data','manifest-loading-list',N'Kê khai hàng hóa',NULL,1,302,'ManifestLoadingList','sql','sql'),
+('input-data','goods-manifest',N'Bảng kê danh mục hàng hóa',NULL,1,303,'GoodsManifest','sql','sql')
+
+INSERT INTO SA_PERMISSION (ROLE_CODE,MENU_CODE,IS_VIEW,IS_ADD_NEW,IS_MODIFY,IS_DELETE,CREATE_BY,UPDATE_BY)
+VALUES 
+('admin','user',1,1,1,1,'sql','sql'),
+('admin','permission',1,1,1,1,'sql','sql'),
+('admin','warehouse-list',1,1,1,1,'sql','sql'),
+('admin','warehouse-design',1,1,1,1,'sql','sql'),
+('admin','gate-list',1,1,1,1,'sql','sql'),
+('admin','equipment-group-list',1,1,1,1,'sql','sql'),
+('admin','equipment-list',1,1,1,1,'sql','sql'),
+('admin','method-list',1,1,1,1,'sql','sql'),
+('admin','item-type',1,1,1,1,'sql','sql'),
+('admin','unit-list',1,1,1,1,'sql','sql'),
+('admin','customer-type',1,1,1,1,'sql','sql'),
+('admin','customer-list',1,1,1,1,'sql','sql'),
+('admin','tractor-list',1,1,1,1,'sql','sql'),
+('admin','trailer-list',1,1,1,1,'sql','sql'),
+('admin','vessel-info',1,1,1,1,'sql','sql'),
+('admin','manifest-loading-list',1,1,1,1,'sql','sql'),
+('admin','goods-manifest',1,1,1,1,'sql','sql'),
+('procedure-staff','user',1,0,0,0,'sql','sql'),
+('procedure-staff','permission',1,0,0,0,'sql','sql'),
+('procedure-staff','warehouse-list',1,1,1,1,'sql','sql'),
+('procedure-staff','warehouse-design',1,1,1,1,'sql','sql'),
+('procedure-staff','gate-list',1,1,1,1,'sql','sql'),
+('procedure-staff','equipment-group-list',1,1,1,1,'sql','sql'),
+('procedure-staff','equipment-list',1,1,1,1,'sql','sql'),
+('procedure-staff','method-list',1,1,1,1,'sql','sql'),
+('procedure-staff','item-type',1,1,1,1,'sql','sql'),
+('procedure-staff','unit-list',1,1,1,1,'sql','sql'),
+('procedure-staff','customer-type',1,1,1,1,'sql','sql'),
+('procedure-staff','customer-list',1,1,1,1,'sql','sql'),
+('procedure-staff','tractor-list',1,1,1,1,'sql','sql'),
+('procedure-staff','trailer-list',1,1,1,1,'sql','sql')
+
+INSERT INTO SA_USER (ROLE_CODE, USER_NAME,CREATE_BY,UPDATE_BY)
+VALUES ('admin','superadmin','sql','sql'),
+VALUES ('procedure-staff','vusiphu','sql','sql')
