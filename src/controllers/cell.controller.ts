2f1de88af9478909938a2b90bb57296f20600402
PenVsse <vusiphu20042000@gmail.com>
2024-07-19 02:18:53 +0700

    fix

--- src/controllers/cell.controller.ts
+++ src/controllers/cell.controller.ts
@@ -6,9 +6,15 @@ import CellService from '../services/cell.service';
 class CellController {
   suggestCell = async (req: Request, res: Response) => {
     const warehouseCode = req.query.warehouseCode as string;
+    const palletInfo = {
+      PALLET_HEIGHT: Number(req.query.PALLET_HEIGHT as string),
+      PALLET_LENGTH: Number(req.query.PALLET_LENGTH as string),
+      PALLET_WIDTH: Number(req.query.PALLET_WIDTH as string),
+    };
+    console.log('🚀 ~ CellController ~ suggestCell= ~ palletInfo:', palletInfo);
     new SuccessResponse({
       message: SUCCESS_MESSAGE.GET_DATA_SUCCESS,
-      metadata: await CellService.suggestCell(req.body, warehouseCode),
+      metadata: await CellService.suggestCell(palletInfo, warehouseCode),
     }).send(res);
   };
 }
